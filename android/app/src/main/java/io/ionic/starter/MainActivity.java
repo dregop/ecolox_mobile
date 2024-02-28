e02203059de16e70ecd740e42c3bd66a4ac8c854
Dregop <pierredrege_2@hotmail.fr>
2024-02-28 02:01:51 +0100

    fix issue build

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,17 +1,5 @@
 package io.ionic.starter;
+
 import com.getcapacitor.BridgeActivity;
-import com.equimaps.capacitor_background_geolocation.BackgroundGeolocation;
 
-public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      // Additional plugins you've installed go here
-      // Ex: add(TotallyAwesomePlugin.class);
-      add(BackgroundGeolocation.class);
-    }});
-  }
-}
+public class MainActivity extends BridgeActivity {}
