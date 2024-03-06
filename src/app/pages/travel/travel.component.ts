847194d003147dedcd8a34e2f797b591eb27b0a4
Dregop <pierredrege_2@hotmail.fr>
2024-03-06 19:43:27 +0100

    add missing permissions

--- src/app/pages/travel/travel.component.ts
+++ src/app/pages/travel/travel.component.ts
@@ -37,94 +37,94 @@ export class TravelComponent implements OnInit {
 
     // this.onDeviceReady();
 
-// To start listening for changes in the device's location, add a new watcher.
-// You do this by calling 'addWatcher' with an options object and a callback. An
-// ID is returned, which can be used to remove the watcher in the future. The
-// callback will be called every time a new location is available. Watchers can
-// not be paused, only removed. Multiple watchers may exist at the same time.
-const watcher_id = BackgroundGeolocation.addWatcher(
-  {
-      // If the "backgroundMessage" option is defined, the watcher will
-      // provide location updates whether the app is in the background or the
-      // foreground. If it is not defined, location updates are only
-      // guaranteed in the foreground. This is true on both platforms.
-
-      // On Android, a notification must be shown to continue receiving
-      // location updates in the background. This option specifies the text of
-      // that notification.
-      backgroundMessage: "Cancel to prevent battery drain.",
-
-      // The title of the notification mentioned above. Defaults to "Using
-      // your location".
-      backgroundTitle: "Tracking You.",
-
-      // Whether permissions should be requested from the user automatically,
-      // if they are not already granted. Defaults to "true".
-      requestPermissions: true,
-
-      // If "true", stale locations may be delivered while the device
-      // obtains a GPS fix. You are responsible for checking the "time"
-      // property. If "false", locations are guaranteed to be up to date.
-      // Defaults to "false".
-      stale: false,
-
-      // The minimum number of metres between subsequent locations. Defaults
-      // to 0.
-      distanceFilter: 50
-  },
-  function callback(location, error) {
-    if (error) {
-        if (error.code === "NOT_AUTHORIZED") {
-            if (window.confirm(
-                "This app needs your location, " +
-                "but does not have permission.\n\n" +
-                "Open settings now?"
-            )) {
-                // It can be useful to direct the user to their device's
-                // settings when location permissions have been denied. The
-                // plugin provides the 'openSettings' method to do exactly
-                // this.
-                BackgroundGeolocation.openSettings();
-            }
-        }
-        return console.error(error);
-    }
-
-    return console.log(location);
-}
-).then(function after_the_watcher_has_been_added(watcher_id) {
-// When a watcher is no longer needed, it should be removed by calling
-// 'removeWatcher' with an object containing its ID.
-BackgroundGeolocation.removeWatcher({
-    id: watcher_id
-});
-});
-
-// If you just want the current location, try something like this. The longer
-// the timeout, the more accurate the guess will be. I wouldn't go below about
-// 100ms.
-function guess_location(callback: any, timeout: number) {
-  let last_location: any;
-  BackgroundGeolocation.addWatcher(
+    // To start listening for changes in the device's location, add a new watcher.
+    // You do this by calling 'addWatcher' with an options object and a callback. An
+    // ID is returned, which can be used to remove the watcher in the future. The
+    // callback will be called every time a new location is available. Watchers can
+    // not be paused, only removed. Multiple watchers may exist at the same time.
+    const watcher_id = BackgroundGeolocation.addWatcher(
       {
-          requestPermissions: false,
-          stale: true
+          // If the "backgroundMessage" option is defined, the watcher will
+          // provide location updates whether the app is in the background or the
+          // foreground. If it is not defined, location updates are only
+          // guaranteed in the foreground. This is true on both platforms.
+
+          // On Android, a notification must be shown to continue receiving
+          // location updates in the background. This option specifies the text of
+          // that notification.
+          backgroundMessage: "Cancel to prevent battery drain.",
+
+          // The title of the notification mentioned above. Defaults to "Using
+          // your location".
+          backgroundTitle: "Tracking You.",
+
+          // Whether permissions should be requested from the user automatically,
+          // if they are not already granted. Defaults to "true".
+          requestPermissions: true,
+
+          // If "true", stale locations may be delivered while the device
+          // obtains a GPS fix. You are responsible for checking the "time"
+          // property. If "false", locations are guaranteed to be up to date.
+          // Defaults to "false".
+          stale: false,
+
+          // The minimum number of metres between subsequent locations. Defaults
+          // to 0.
+          distanceFilter: 50
       },
-      function (location) {
-          last_location = location || undefined;
-          const speedSpan = document.getElementById('speed');
-          if (speedSpan && last_location) {
-            speedSpan.innerHTML = last_location.coords.speed;
+      function callback(location, error) {
+        if (error) {
+            if (error.code === "NOT_AUTHORIZED") {
+                if (window.confirm(
+                    "This app needs your location, " +
+                    "but does not have permission.\n\n" +
+                    "Open settings now?"
+                )) {
+                    // It can be useful to direct the user to their device's
+                    // settings when location permissions have been denied. The
+                    // plugin provides the 'openSettings' method to do exactly
+                    // this.
+                    BackgroundGeolocation.openSettings();
+                }
+            }
+            return console.error(error);
+        }
+
+        return console.log(location);
+    }
+    ).then(function after_the_watcher_has_been_added(watcher_id) {
+    // When a watcher is no longer needed, it should be removed by calling
+    // 'removeWatcher' with an object containing its ID.
+    BackgroundGeolocation.removeWatcher({
+        id: watcher_id
+    });
+    });
+
+    // If you just want the current location, try something like this. The longer
+    // the timeout, the more accurate the guess will be. I wouldn't go below about
+    // 100ms.
+    function guess_location(callback: any, timeout: number) {
+      let last_location: any;
+      BackgroundGeolocation.addWatcher(
+          {
+              requestPermissions: false,
+              stale: true
+          },
+          function (location) {
+              last_location = location || undefined;
+              const speedSpan = document.getElementById('speed');
+              if (speedSpan && last_location) {
+                speedSpan.innerHTML = last_location.coords.speed;
+              }
           }
-      }
-  ).then(function (id) {
-      setTimeout(function () {
-          callback(last_location);
-          BackgroundGeolocation.removeWatcher({id});
-      }, timeout);
-  });
-  }
-  guess_location;
+      ).then(function (id) {
+          setTimeout(function () {
+              callback(last_location);
+              BackgroundGeolocation.removeWatcher({id});
+          }, timeout);
+      });
+    }
+    guess_location;
   }
 
   public displayChallenges() {
