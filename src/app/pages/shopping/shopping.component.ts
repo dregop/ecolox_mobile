7613d7164db0be9b306177b7a2eef433db31065f
Dregop <pierredrege_2@hotmail.fr>
2024-05-14 16:53:01 +0200

    fix shopping

--- src/app/pages/shopping/shopping.component.ts
+++ src/app/pages/shopping/shopping.component.ts
@@ -128,7 +128,7 @@ export class ShoppingComponent implements OnInit {
     let newData: string[] = [];
     const names = dataFromDb['Nom du Produit en Français'];
     const co2ByKg = dataFromDb['kg CO2 eq/kg de produit'];
-    this.debug = 'le nom : ' + names[0];
+    // this.debug = 'le nom : ' + names[0];
     for (const [key, value] of Object.entries(names)) {
       newData.push(value as string);
       console.log({name: value, co2ByKg: co2ByKg[key]});
@@ -152,6 +152,7 @@ export class ShoppingComponent implements OnInit {
       }));
 
       search$.subscribe(data => {
+        this.debug = JSON.stringify(data);
         this.isSearching = false
         this.searchedProduct = this.handleData(data);
       });
